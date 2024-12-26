01f21f117a5c6ad0ea8dbb0a6573ead08551b97e
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-12-26 11:41:58 +0900

    init

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,12 +2,14 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@ServletComponentScan
 @SpringBootApplication
-public class ServletApplication {
+public class ServletApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
 }
