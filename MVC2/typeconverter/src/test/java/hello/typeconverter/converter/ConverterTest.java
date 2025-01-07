17d82442f79204c391ebe432ab4c018d5217a296
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-01-07 16:51:26 +0900

    25.01.07 : Spring Converter Interface 사용

--- MVC2/typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ MVC2/typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.converter;
 
+import hello.typeconverter.type.IpPort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,4 +20,19 @@ public class ConverterTest {
         Assertions.assertThat(result).isEqualTo("10");
     }
 
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        Assertions.assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        Assertions.assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
 }
